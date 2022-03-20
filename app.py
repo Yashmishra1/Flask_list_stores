01b0b5dab610368694470fd70abbe90d21da583d
Yashmishra1 <69563845+Yashmishra1@users.noreply.github.com>
2022-03-21 01:29:10 +0530

    👽️ Add api endpoints

--- app.py
+++ app.py
@@ -1,9 +1,43 @@
-from flask import Flask
+from flask import Flask,jsonify
 
 app = Flask(__name__)
+#POST = used to receive data 
+#GET = used to send data back only
 
-@app.route('/')
-def Home():
-    return "Hello World"
+stores = [
+     {
+    'name':'my Wonderful store',
+    'items': [
+        {
+        'name':'My item',
+        'price': 989
+        }
+    ]
+}
+]
+#POST /store data :{name}
+@app.route('/store', methods=['POST'])
+def create_store():
+    pass
+
+#GET /store/<string:name>
+@app.route('/store/<string:name>', methods=['GET'])
+def get_store(name):
+    pass
+
+#Get store
+@app.route('/store', methods=['GET'])
+def get_Stores():
+    return jsonify({'stores': stores})
+    
+#POST /store/string:name>/item {name:price}
+@app.route('/store/<string:name>/item', methods=['POST'])
+def create_item(name):
+    pass
+
+#GET /store/<string:name/item
+@app.route('/store/<string:name>/item', methods=['GET'])
+def get_items_in_store(name):
+    pass
 
 app.run(port=5000)
